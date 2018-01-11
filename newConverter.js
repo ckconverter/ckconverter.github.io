dff5eba92966ab7ee26becc600d227a53a8b23bc
ckconverter <xymox113@gmail.com>
2018-01-11 13:16:28 -0500

    Update newConverter.js

--- newConverter.js
+++ newConverter.js
@@ -3,3 +3,21 @@ function clickOn(clickOnThing,clickCode) {
 		clickCode
 	});
 }
+
+//hide or show stuff functions
+function hideThis(thingToHide) {
+	$(thingToHide).hide();
+}
+function showThis(thingToShow) {
+	$(thingToShow).show();
+}
+
+$(document).ready(function() {
+	//hiding stuff at the beginning
+	hideThis('#back');
+	hideThis('#fromHeading');
+	hideThis('#toHeading');
+	hideThis('.temperature');
+	hideThis('.distance');
+	hideThis('.pressure');
+});
